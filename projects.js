ab0545beb45870564abf93887c0762f4adae1364
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-02 12:20:13 -0500

    created array of project objects

--- projects.js
+++ projects.js
@@ -1,1 +1,43 @@
 console.log('Personal Site Yo');
+
+const projects = [
+    {title: "First Project", 
+    screenshot: "https://www.elegantthemes.com/blog/wp-content/uploads/2016/06/metricool.jpg", 
+    description: "This is a project using learned technologies to showcase mastery, constructed using the normal methods.", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/williaj615", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/williaj615"},
+
+    {title: "Second Project", 
+    screenshot: "https://www.elegantthemes.com/blog/wp-content/uploads/2016/06/metricool.jpg", 
+    description: "This is a project using learned technologies to showcase mastery, constructed using the new methods.", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/williaj615", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/williaj615"},
+
+    {title: "Third Project", 
+    screenshot: "https://www.elegantthemes.com/blog/wp-content/uploads/2016/06/metricool.jpg", 
+    description: "This is a project using learned technologies to showcase mastery, constructed using the newer methods.", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/williaj615", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/williaj615"},
+
+    {title: "Fourth Project", 
+    screenshot: "https://www.elegantthemes.com/blog/wp-content/uploads/2016/06/metricool.jpg", 
+    description: "This is a project using learned technologies to showcase mastery, constructed using the newest methods.", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/williaj615", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/williaj615"},
+
+    {title: "Fifth Project", 
+    screenshot: "https://www.elegantthemes.com/blog/wp-content/uploads/2016/06/metricool.jpg", 
+    description: "This is a project using learned technologies to showcase mastery, constructed using some crazy methods.", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/williaj615", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/williaj615"}
+]

0f38e8c6cd2c7c9b07f0514a1c6273c1ed97a22c
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-12-28 01:08:08 -0600

    WIP

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,7 @@
 import projects from './projects';
 import '../styles/main.scss';
-import navbar from './MyNavBar/myNavBar';
 
 const init = () => {
-  // projects.addClickEvents();
-  navbar.printNavBar();
   projects.buildProjectCards();
 };
 
